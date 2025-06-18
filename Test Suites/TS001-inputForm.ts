cf74a6af0b449291dd813ea1e613a9f261d5bb76
entrustinv132 <entrustinv132@localhost>
2025-06-18 20:49:47 +0700

    3 commit

---
+++ Test Suites/TS001-inputForm.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS001-inputForm</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ed6b5f8a-7592-4cf1-bd3d-df2ee841d2c7</testSuiteGuid>
+   <testCaseLink>
+      <guid>e6892848-9c4d-4215-a7a4-2f372cb15991</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/001-inputForm</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
